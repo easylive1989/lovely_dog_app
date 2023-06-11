04518a2a1962cd378c4b9b4b36ee5024b636d95f
Paul <easylive1989@gmail.com>
2023-06-11 14:30:43 +0800

    clear

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:lovely_dog_app/breed_list_page.dart';
-import 'package:lovely_dog_app/dog_api.dart';
-import 'package:lovely_dog_app/dog_image_page.dart';
 
 void main() {
-  runApp(MainApp(dogApi: DogApi(client: Client())));
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({
-    super.key,
-    required this.dogApi,
-  });
-
-  final DogApi dogApi;
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      onGenerateRoute: (settings) {
-        if (settings.name == DogImagePage.routeName) {
-          return MaterialPageRoute(
-            builder: (context) => DogImagePage(
-              dogApi: dogApi,
-              breed: settings.arguments as String,
-            ),
-          );
-        }
-        return null;
-      },
-      home: BreedListPage(dogApi: dogApi),
+    return const MaterialApp(
+      home: SizedBox(),
     );
   }
 }
